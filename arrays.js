59cdae39873157f65620e2ab8d7b76664c108634
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-01 20:04:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
-  array
+  return array
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
